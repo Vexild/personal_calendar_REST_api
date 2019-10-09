755528e67f46b6439c55b9728de5e99fab88db80
vexild <vili.ahonen@hotmail.com>
2019-10-09 22:59:04 +0300

    small fixing

--- calendar.js
+++ calendar.js
@@ -16,6 +16,8 @@ mongoose.connect(process.env.DBCON,
   { useUnifiedTopology: true },
   () => console.log("Connected to database.")
 );
+
+// lets setup our very own schema for events
 var Schema = mongoose.Schema;
 var eventSchema = new Schema ({
   event_name: {
@@ -33,9 +35,11 @@ var eventSchema = new Schema ({
   }
 },{collection: "calendar", versionKey: false});
 
+// Our Event model
 var Event = mongoose.model('eventModel', eventSchema);
 module.exports = Event;
 
+// we set app to localhost:3000
 app.listen(3000, function () {
   console.log('Calendar app listening on port 3000!');
 });
@@ -67,11 +71,9 @@ app.post('/new_event', function(req, res) {
 
 // DELETE EVENT
 app.delete('/delete_event/:id', function (req, res) {
-  console.log(req.params.id)
   Event.deleteOne({'_id' : req.params.id}, function (err,par){
     if(err) throw err;
     res.set('Access-Control-Allow-Origin','*');
-    //res.send("Deleted "+ req.params.id)
     res.send(par)
   });
   
@@ -82,8 +84,7 @@ app.get('/events/:id', function (req, res) {
   Event.find({'event_name':req.params.id}, function (err, par) {
     if (err) throw err;
       
-    // To calculate the time difference of two dates 
-    var Difference_In_Time = par[0].ending_date - par[0].starting_date; 
+    // Lets calculate time difference with moment module
     var date1 = moment((par[0].starting_date).toUTCString())
     var date2 = moment((par[0].ending_date).toUTCString())
     var difference = moment.utc(moment(date2,"DD/MM/YYYY HH:mm:ss").diff(moment(date1,"DD/MM/YYYY HH:mm:ss"))).format("DD HH:mm:ss")    
@@ -98,7 +99,6 @@ app.put('/modify_event/:id', function (req, res) {
   var data = {event_name: req.body.eventName,
     starting_date: req.body.starting_date,
     ending_date: req.body.ending_date}
-  console.log(id+" & "+data)
   Event.updateOne(id, data, function(err, par) {
     if(err) throw err;
     res.send(par)
